0e04041c992f4eecb188a6c7fafe5832e495bfea
uig22566 <neng.xu@aumovio.com>
2026-08-12 09:53:06 +0800

    updated

--- scenes/Background/StopLineArea.gd
+++ scenes/Background/StopLineArea.gd
@@ -1,14 +1,16 @@
 extends Area2D
 
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_area_entered(area: Area2D) -> void:
 	print('到停止线了')
-	if body.is_in_group("cars") and body.has_method("set_at_stopline"):
-		body.set_at_stopline(true)
+	var car = area.get_parent()
+	if car.is_in_group("cars") and car.has_method("set_at_stopline"):
+		car.set_at_stopline(true)
 
-func _on_body_exited(body: Node2D) -> void:
-	if body.is_in_group("cars") and body.has_method("set_at_stopline"):
-		body.set_at_stopline(false)
+func _on_area_exited(area: Area2D) -> void:
+	var car = area.get_parent()
+	if car.is_in_group("cars") and car.has_method("set_at_stopline"):
+		car.set_at_stopline(false)
