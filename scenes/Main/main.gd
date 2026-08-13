9374217ef7fd2c3cb7fbeac4b5c1370beefd8414
uig22566 <neng.xu@aumovio.com>
2026-08-13 10:26:26 +0800

    updated

--- scenes/Main/main.gd
+++ scenes/Main/main.gd
@@ -5,7 +5,7 @@ extends Node2D
 @onready var pedestrian_spawner: Node = $PedestrianSpawner
 @onready var pedestrians_container: Node2D = $PedestriansContainer   # 如果你还留着这个容器；不留的话可以直接用 Main 自己
 
-@onready var BackgroundMusic: AudioStreamPlayer2D = $BackgroundMusic
+@onready var BackgroundMusic: AudioStreamPlayer = $BackgroundMusic
 
 func _ready() -> void:
 	BackgroundMusic.play()
