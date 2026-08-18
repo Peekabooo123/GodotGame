03354a03ee85bc7e978340876664000ea99ef771
xnsssb <603334190@qq.com>
2026-08-18 13:30:57 +0800

    zebracrossing

--- scenes/Main/intersection.gd
+++ scenes/Main/intersection.gd
@@ -1,3 +1,4 @@
+@tool
 extends Node2D
 
 @onready var TrafficLight_left = $TrafficLight_left
@@ -8,3 +9,22 @@ extends Node2D
 func _ready() -> void:
 	TrafficLight_left.setup(controller)
 	TrafficLight_right.setup(controller)
+	
+	
+func _draw() -> void:
+	_zebra_crossing_drawing()
+	pass
+
+func _zebra_crossing_drawing() -> void:
+	var strip_width = 20
+	var strip_height = 130
+	var gap = 10
+	var position = Vector2(300,300)
+
+
+	var strip_total_width = strip_width + gap
+	var strip_count = 12
+
+	for i in range(strip_count+1):
+		var x = position.x +  i * strip_total_width
+		draw_rect(Rect2(Vector2(x, position.y), Vector2(strip_width, strip_height)), Color.WHITE)
