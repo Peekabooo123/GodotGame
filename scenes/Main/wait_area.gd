d9c7db15f9542d3e58ea86a23b6d451d51c14c4d
xnsssb <603334190@qq.com>
2026-08-21 16:57:44 +0800

    update

--- scenes/Main/wait_area.gd
+++ scenes/Main/wait_area.gd
@@ -1,9 +1,11 @@
 extends Area2D
 var controller = null
 
+@export var traffic_light_controller: Node2D   # 对应的红绿灯控制器
+@export var intersection: Node2D        # 拖入 Intersection 根节点
+@export var opposite_area: Area2D       # 拖入对面的 WaitArea
 
-func setup(p_controller: Node2D) -> void:
-	controller = p_controller
+
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -11,8 +13,10 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group('pedestrians'):
+
 		if body.has_method("set_current_intersection"):
-			body.set_current_intersection(controller)
+			body.set_current_intersection(traffic_light_controller)
+
 		if body.has_method("offer_crossing_decision"):
 			body.offer_crossing_decision(self)
 
