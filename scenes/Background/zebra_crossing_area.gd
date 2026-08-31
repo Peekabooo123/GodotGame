9cd2de4499363974d07817b5da77ca45861a7a37
uig22566 <603334190@qq.com>
2026-08-31 11:49:25 +0800

    update

--- scenes/Background/zebra_crossing_area.gd
+++ scenes/Background/zebra_crossing_area.gd
@@ -1,9 +1,7 @@
 extends Area2D
-var pedestrians_on_crossing: int = 0
-var controller: Node2D = null
+@export var traffic_light_controller: Node2D   # 对应的红绿灯控制器
 
-func setup(p_controller: Node2D) -> void:
-	controller = p_controller
+var pedestrians_on_crossing: int = 0
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
@@ -12,23 +10,17 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("pedestrians"):
-		pedestrians_on_crossing += 1
 		body.set_on_crosswalk(true)
-		TrafficJudge.report_crosswalk_entry(body, controller.is_red())
-		if pedestrians_on_crossing == 1:
-			#Eventbus.crosswalk_occupancy_changed.emit(true)
-			print('you ren')
+		TrafficJudge.report_crosswalk_entry(body, traffic_light_controller.is_red())
+
 	if body.is_in_group('cars'):
 		pass
 		#print('Cars in')
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("pedestrians"):
-		pedestrians_on_crossing -= 1
 		body.set_on_crosswalk(false)
-		if pedestrians_on_crossing == 0:
-			#Eventbus.crosswalk_occupancy_changed.emit(false)
-			pass
+
 	if body.is_in_group('cars'):
 		#print('Cars out')
 		pass
